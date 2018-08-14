e9c881d1896f9bcab53c04b6a155d7257f77152c
Michael Xueyuan Han <hanx@g.harvard.edu>
2018-08-14 10:59:20 +0100

    processing code and data

--- streaming/data/preprocess-ss.py
+++ streaming/data/preprocess-ss.py
@@ -3,7 +3,7 @@ import math
 
 def read_single_graph(file_name, graph_id):
 	'''
-	Read a single graph with ID @graph_id from the file @file_name and return the list of its edges.
+	Read a graph with ID in @graph_id from the file @file_name and return the list of its edges.
 	'''
 	graph = []	# Store graph edges.
 	node_id_seen = []	# Whether this node ID has been seen before.
@@ -11,7 +11,7 @@ def read_single_graph(file_name, graph_id):
 	with open(file_name) as f:
 		for line in f:
 			edge = line.strip().split("\t")
-			if edge[5] == graph_id:
+			if edge[5] in graph_id:
 				if edge[0] in node_id_seen:	# Check if source ID has been seen before.
 					edge.append("0")
 				else:
@@ -30,26 +30,34 @@ def read_single_graph(file_name, graph_id):
 
 def print_instruction():
 	print(
-		"Usage: python preprocess-ss.py <graph_id> <input_file_path> <base_graph_file_path> <stream_file_path>\n"
-		"The first 10% of the edges in the graph will be considered as the basis of the streaming graph, stored in <base_graph_file_path>.\n"
-		"The rest of the edges will be streamed in, stored in <stream_file_path>.\n")
+		"Usage: python preprocess-ss.py <base_graph_id_start> <base_graph_id_end> <stream_graph_id_start> <stream_graph_id_end> <input_file_path> <base_graph_file_path> <stream_file_path>\n")
 
 if __name__ == "__main__":
-	if (len(sys.argv) < 5):
+	if (len(sys.argv) < 8):
 		print_instruction()
 		sys.exit(1)
 
-	graph = read_single_graph(sys.argv[2], sys.argv[1])
-	base_graph_size = math.ceil(len(graph) * 0.1)	# The size of base graph.
-	stream_graph_size = len(graph) - base_graph_size
+	base_graph_id_start = sys.argv[1]
+	base_graph_id_end = sys.argv[2]
+	stream_graph_id_start = sys.argv[3]
+	stream_graph_id_end = sys.argv[4]
+	base_graph_ids = []
+	stream_graph_ids = []
+	total_ids = []
+	for i in range(int(base_graph_id_start), int(base_graph_id_end) + 1):
+		base_graph_ids.append(str(i))
+		total_ids.append(str(i))
+	for i in range(int(stream_graph_id_start), int(stream_graph_id_end) + 1):
+		stream_graph_ids.append(str(i))
+		total_ids.append(str(i))
 
-	base_file = open(sys.argv[3], "w")
-	stream_file = open(sys.argv[4], "w")
+	graph = read_single_graph(sys.argv[5], total_ids)
 
-	cnt = 0
+	base_file = open(sys.argv[6], "w")
+	stream_file = open(sys.argv[7], "w")
+
 	for edge in graph:
-		if cnt < base_graph_size:
-			cnt = cnt + 1
+		if edge[5] in base_graph_ids:
 			base_file.write(str(edge[0]) + " " + str(edge[2]) + " " + edge[1] + ":" + edge[3] + ":" + edge[4] + ":" + str(edge[8]) + "\n")
 		else:
 			stream_file.write(str(edge[0]) + " " + str(edge[2]) + " " + edge[1] + ":" + edge[3] + ":" + edge[4] + ":" + edge[6] + ":" + edge[7] + ":" + str(edge[8]) + "\n")
