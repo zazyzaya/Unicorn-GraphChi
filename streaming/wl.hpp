7adc1b8ceb097fdb147350feed913332b7f98aae
Michael Xueyuan Han <hanx@g.harvard.edu>
2018-10-07 15:08:10 +0100

    [ci skip] try to fix a synchronization bug.

--- streaming/wl.hpp
+++ streaming/wl.hpp
@@ -539,6 +539,7 @@ namespace graphchi {
 				if (std::stop) {
 					logstream(LOG_DEBUG) << "Everything is done!" << std::endl;
 					gcontext.set_last_iteration(iteration);/* Set this iteration as the last one. */
+					pthread_barrier_wait(&std::graph_barrier);
 					return;
 				}
 				pthread_barrier_wait(&std::stream_barrier);
